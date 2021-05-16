fc28b3784c9d760ba9809dd04f07d2c8ccc1ca5b
Solomiya_Yaremko <sol.yaremko@gmail.com>
2021-05-17 00:53:57 +0300

    Dish/Order Services tests modifications

--- src/test/java/ua/training/service/DishServiceTest.java
+++ src/test/java/ua/training/service/DishServiceTest.java
@@ -1,16 +1,34 @@
 package ua.training.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static ua.training.service.DishService.CREATE_DISH;
+import static ua.training.service.DishService.DELETE_DISH;
+import static ua.training.service.DishService.GET_ALL_DISHES;
+import static ua.training.service.DishService.GET_DISH_BY_ID;
+import static ua.training.service.DishService.SEARCH_DISHES_BY_CATEGORY_NAME;
+import static ua.training.service.DishService.SEARCH_DISHES_BY_NAME;
+import static ua.training.service.DishService.SEARCH_MOST_POPULAR_DISH_PER_PERIOD;
+import static ua.training.service.DishService.UPDATE_DISH;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
 import ua.training.converter.DishDtoDishConverter;
 import ua.training.dao.DaoFactory;
 import ua.training.dao.DishDao;
@@ -18,152 +36,146 @@ import ua.training.dto.DishDto;
 import ua.training.entity.Dish;
 import ua.training.testData.DishTestDataGenerator;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-import static ua.training.service.DishService.*;
-
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({LogManager.class, DaoFactory.class, DishService.class})
+@PrepareForTest({ LogManager.class, DaoFactory.class, DishService.class })
 public class DishServiceTest {
 
-    private static Logger LOGGER;
+	private static Logger LOGGER;
 
-    @Mock
-    private DaoFactory daoFactory;
-    @Mock
-    private DishDao dishDao;
+	@Mock
+	private DaoFactory daoFactory;
+	@Mock
+	private DishDao dishDao;
 
-    private DishService dishService;
+	private DishService dishService;
 
-    @BeforeClass
-    public static void setUp() {
-        LOGGER = PowerMockito.mock(Logger.class);
-        PowerMockito.mockStatic(LogManager.class);
-        PowerMockito.when(LogManager.getLogger(DishService.class)).thenReturn(LOGGER);
-    }
+	@BeforeClass
+	public static void setUp() {
+		LOGGER = PowerMockito.mock(Logger.class);
+		PowerMockito.mockStatic(LogManager.class);
+		PowerMockito.when(LogManager.getLogger(DishService.class)).thenReturn(LOGGER);
+	}
 
-    @Before
-    public void setUpBeforeMethod() {
-        dishService = new DishService(daoFactory);
-        when(daoFactory.createDishDao()).thenReturn(dishDao);
-    }
+	@Before
+	public void setUpBeforeMethod() {
+		dishService = new DishService(daoFactory);
+		when(daoFactory.createDishDao()).thenReturn(dishDao);
+	}
 
-    @Test
-    public void shouldReturnDishServiceInstanceOnGetInstance() throws Exception {
-        PowerMockito.mockStatic(DaoFactory.class);
-        PowerMockito.when(DaoFactory.getDaoFactory()).thenReturn(daoFactory);
-        PowerMockito.whenNew(DishService.class).withArguments(daoFactory).thenReturn(dishService);
+	// should ... when ... on
+	// given ... when ... then
+	@Test
+	public void shouldReturnDishServiceInstanceOnGetInstance() throws Exception {
+		PowerMockito.mockStatic(DaoFactory.class);
+		PowerMockito.when(DaoFactory.getDaoFactory()).thenReturn(daoFactory);
+		PowerMockito.whenNew(DishService.class).withArguments(daoFactory).thenReturn(dishService);
 
-        DishService.getInstance();
+		DishService.getInstance();
 
-        PowerMockito.verifyNew(DishService.class).withArguments(daoFactory);
-    }
+		PowerMockito.verifyNew(DishService.class).withArguments(daoFactory);
+	}
 
-    @Test
-    public void shouldReturnDishesOnGetAllDishes() {
-        List<Dish> expectedResult = DishTestDataGenerator.generateDishesList();
-        when(dishDao.getAll()).thenReturn(expectedResult);
+	@Test
+	public void shouldReturnDishesOnGetAllDishes() {
+		List<Dish> expectedResult = DishTestDataGenerator.generateDishesList();
+		when(dishDao.getAll()).thenReturn(expectedResult);
 
-        List<Dish> actualResult = dishService.getAllDishes();
+		List<Dish> actualResult = dishService.getAllDishes();
 
-        assertEquals(expectedResult, actualResult);
-        verify(LOGGER, times(1)).info(GET_ALL_DISHES);
-        verify(daoFactory).createDishDao();
-        verify(dishDao).getAll();
-    }
+		assertEquals(expectedResult, actualResult);
+		verify(LOGGER, times(1)).info(GET_ALL_DISHES);
+		verify(daoFactory).createDishDao();
+		verify(dishDao).getAll();
+	}
 
-    @Test
-    public void shouldReturnDishOnGetDishById() {
-        Optional<Dish> expectedResult = DishTestDataGenerator.generateOptionalDish();
-        Long dishId = 1L;
-        when(dishDao.getById(dishId)).thenReturn(expectedResult);
+	@Test
+	public void shouldReturnDishOnGetDishById() {
+		Optional<Dish> expectedResult = DishTestDataGenerator.generateOptionalDish();
+		Long dishId = 1L;
+		when(dishDao.getById(dishId)).thenReturn(expectedResult);
 
-        Optional<Dish> actualResult = dishService.getDishById(dishId);
+		Optional<Dish> actualResult = dishService.getDishById(dishId);
 
-        assertEquals(expectedResult.get(), actualResult.get());
-        verify(LOGGER, times(1)).info(String.format(GET_DISH_BY_ID, dishId));
-        verify(daoFactory).createDishDao();
-        verify(dishDao).getById(dishId);
-    }
+		assertEquals(expectedResult.get(), actualResult.get());
+		verify(LOGGER, times(1)).info(String.format(GET_DISH_BY_ID, dishId));
+		verify(daoFactory).createDishDao();
+		verify(dishDao).getById(dishId);
+	}
 
-    @Test
-    public void shouldCreateDishOnCreateDish() {
-        DishDto dishDto = DishTestDataGenerator.generateDishForCreation();
+	@Test
+	public void shouldCreateDishOnCreateDish() {
+		DishDto dishDto = DishTestDataGenerator.generateDishForCreation();
 
-        dishService.createDish(dishDto);
+		dishService.createDish(dishDto);
 
-        verify(LOGGER, times(1)).info(String.format(CREATE_DISH, dishDto.getName()));
-        verify(daoFactory).createDishDao();
-        verify(dishDao).create(DishDtoDishConverter.toDish(dishDto));
-    }
+		verify(LOGGER, times(1)).info(String.format(CREATE_DISH, dishDto.getName()));
+		verify(daoFactory).createDishDao();
+		verify(dishDao).create(DishDtoDishConverter.toDish(dishDto));
+	}
 
-    @Test
-    public void shouldUpdateDishOnUpdateDish() {
-        DishDto dishDto = DishTestDataGenerator.generateDishForUpdate();
+	@Test
+	public void shouldUpdateDishOnUpdateDish() {
+		DishDto dishDto = DishTestDataGenerator.generateDishForUpdate();
 
-        dishService.updateDish(dishDto);
+		dishService.updateDish(dishDto);
 
-        verify(LOGGER, times(1)).info(String.format(UPDATE_DISH, dishDto.getId()));
-        verify(daoFactory).createDishDao();
-        verify(dishDao).update(DishDtoDishConverter.toDish(dishDto));
-    }
+		verify(LOGGER, times(1)).info(String.format(UPDATE_DISH, dishDto.getId()));
+		verify(daoFactory).createDishDao();
+		verify(dishDao).update(DishDtoDishConverter.toDish(dishDto));
+	}
 
-    @Test
-    public void shouldDeleteDishOnDeleteDish() {
-        Long dishId = 1L;
+	@Test
+	public void shouldDeleteDishOnDeleteDish() {
+		Long dishId = 1L;
 
-        dishService.deleteDish(dishId);
+		dishService.deleteDish(dishId);
 
-        verify(LOGGER, times(1)).info(String.format(DELETE_DISH, dishId));
-        verify(daoFactory).createDishDao();
-        verify(dishDao).delete(dishId);
-    }
+		verify(LOGGER, times(1)).info(String.format(DELETE_DISH, dishId));
+		verify(daoFactory).createDishDao();
+		verify(dishDao).delete(dishId);
+	}
 
-    @Test
-    public void shouldSearchDishOnSearchDishesByName() {
-        List<Dish> expectedResult = DishTestDataGenerator.generateDishesForSearch();
-        String name = "cheesecake";
-        when(dishDao.searchDishByName(name)).thenReturn(expectedResult);
+	@Test
+	public void shouldSearchDishOnSearchDishesByName() {
+		List<Dish> expectedResult = DishTestDataGenerator.generateDishesForSearch();
+		String name = "cheesecake";
+		when(dishDao.searchDishByName(name)).thenReturn(expectedResult);
 
-        List<Dish> actualResult = dishService.searchDishesByName(name);
+		List<Dish> actualResult = dishService.searchDishesByName(name);
 
-        assertEquals(expectedResult, actualResult);
-        verify(LOGGER, times(1)).info(String.format(SEARCH_DISHES_BY_NAME, name));
-        verify(daoFactory).createDishDao();
-        verify(dishDao).searchDishByName(name);
-    }
+		assertEquals(expectedResult, actualResult);
+		verify(LOGGER, times(1)).info(String.format(SEARCH_DISHES_BY_NAME, name));
+		verify(daoFactory).createDishDao();
+		verify(dishDao).searchDishByName(name);
+	}
 
-    @Test
-    public void shouldSearchDishOnSearchDishesByCategoryName() {
-        List<Dish> expectedResult = DishTestDataGenerator.generateDishesForSearch();
-        String categoryName = "dessert";
-        when(dishDao.searchDishByCategoryName(categoryName)).thenReturn(expectedResult);
+	@Test
+	public void shouldSearchDishOnSearchDishesByCategoryName() {
+		List<Dish> expectedResult = DishTestDataGenerator.generateDishesForSearch();
+		String categoryName = "dessert";
+		when(dishDao.searchDishByCategoryName(categoryName)).thenReturn(expectedResult);
 
-        List<Dish> actualResult = dishService.searchDishesByCategoryName(categoryName);
+		List<Dish> actualResult = dishService.searchDishesByCategoryName(categoryName);
 
-        assertEquals(expectedResult, actualResult);
-        verify(LOGGER, times(1)).info(String.format(SEARCH_DISHES_BY_CATEGORY_NAME, categoryName));
-        verify(daoFactory).createDishDao();
-        verify(dishDao).searchDishByCategoryName(categoryName);
-    }
+		assertEquals(expectedResult, actualResult);
+		verify(LOGGER, times(1)).info(String.format(SEARCH_DISHES_BY_CATEGORY_NAME, categoryName));
+		verify(daoFactory).createDishDao();
+		verify(dishDao).searchDishByCategoryName(categoryName);
+	}
 
-    @Test
-    public void shouldSearchDishOnSearchMostPopularDishesPerPeriod() {
-        List<Dish> expectedResult = DishTestDataGenerator.generateDishesForSearch();
-        LocalDate fromDate = LocalDate.of(2020, Month.MAY, 1);
-        LocalDate toDate = LocalDate.of(2020, Month.MAY, 29);
-        when(dishDao.searchMostPopularDishesPerPeriod(fromDate, toDate)).thenReturn(expectedResult);
+	@Test
+	public void shouldSearchDishOnSearchMostPopularDishesPerPeriod() {
+		List<Dish> expectedResult = DishTestDataGenerator.generateDishesForSearch();
+		LocalDate fromDate = LocalDate.of(2020, Month.MAY, 1);
+		LocalDate toDate = LocalDate.of(2020, Month.MAY, 29);
+		when(dishDao.searchMostPopularDishesPerPeriod(fromDate, toDate)).thenReturn(expectedResult);
 
-        List<Dish> actualResult = dishService.searchMostPopularDishesPerPeriod(fromDate, toDate);
+		List<Dish> actualResult = dishService.searchMostPopularDishesPerPeriod(fromDate, toDate);
 
-        assertEquals(expectedResult, actualResult);
-        verify(LOGGER, times(1)).info(String.format(SEARCH_MOST_POPULAR_DISH_PER_PERIOD, fromDate.toString(), toDate.toString()));
-        verify(daoFactory).createDishDao();
-        verify(dishDao).searchMostPopularDishesPerPeriod(fromDate, toDate);
-    }
+		assertEquals(expectedResult, actualResult);
+		verify(LOGGER, times(1))
+				.info(String.format(SEARCH_MOST_POPULAR_DISH_PER_PERIOD, fromDate.toString(), toDate.toString()));
+		verify(daoFactory).createDishDao();
+		verify(dishDao).searchMostPopularDishesPerPeriod(fromDate, toDate);
+	}
 }
